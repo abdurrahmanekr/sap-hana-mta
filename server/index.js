2a517ce8d7c5a3d4d1788d70a51ded59ffdc1840
Abdurrahman eker <abdurrahmaneker58@gmail.com>
2019-07-18 10:19:34 +0300

    sap/hana-client bağımlılığı eklendi

--- server/index.js
+++ server/index.js
@@ -2,9 +2,33 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000;
 
+var hdb = require('@sap/hana-client');
+
+var conn_params = {
+    serverNode  : 'myserver:30015',
+    uid         : 'system',
+    pwd         : 'manager'
+};
+
 app.get('/', function (req, res) {
-	console.log(req.db);
-    res.send('Hello World!')
+    try {
+    	conn.connect(conn_params, function(err) {
+            if (err)
+                throw err;
+            
+            conn.exec('SELECT Name, Description FROM Products WHERE id = ?', [1], function (err, result) {
+                if (err)
+                    throw err;
+
+                res.send(JSON.stringify(result));
+
+                conn.disconnect();
+            });
+        });
+    }
+    catch(err) {
+        res.status(500).send(String(err));
+    }
 })
 
 app.listen(port, function () {
